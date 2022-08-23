0e49d3b354d25ec4b66561f94b5dbf5e561f6adf
MarlonHersal <hern.sal.dz@gmail.com>
2022-08-23 16:52:09 -0500

    Ft2 -06 Completado

--- 06-React-Intro/homework/src/components/Card.jsx
+++ 06-React-Intro/homework/src/components/Card.jsx
@@ -2,5 +2,13 @@ import React from 'react';
 
 export default function Card(props) {
   // acá va tu código
-  return <div>Card Component</div>
+  return <div>
+    <button onClick={props.onClose}>X</button>
+    <h6>{props.name}</h6>
+    <div>
+     <p>Min:{props.min}</p> 
+     <p>Max:{props.max}</p>
+     </div>
+    <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="Imagen del las nubes"></img>
+  </div>
 };
--- 06-React-Intro/homework/src/components/SearchBar.jsx
+++ 06-React-Intro/homework/src/components/SearchBar.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 
 export default function SearchBar(props) {
   // acá va tu código
-  return <div>Search Bar Component</div>
+  return <div><input type="text" />
+  <button onClick={() => alert("ciudad")}>Agregar</button></div>
 };
